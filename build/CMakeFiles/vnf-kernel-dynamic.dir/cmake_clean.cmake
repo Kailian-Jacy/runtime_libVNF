1180f0914c22149a3a8e95b86ea9b4a1c3af6eb4
kailian jacy <kailian.jacy@gmai.com>
2024-03-04 11:33:24 +0000

    init: primary version of modified libVNF.

---
+++ build/CMakeFiles/vnf-kernel-dynamic.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vnf-kernel-dynamic.dir/src/datastore/dspackethandler.cpp.o"
+  "CMakeFiles/vnf-kernel-dynamic.dir/src/datastore/utils.cpp.o"
+  "CMakeFiles/vnf-kernel-dynamic.dir/src/kernel/core.cpp.o"
+  "libvnf-kernel-dynamic.pdb"
+  "libvnf-kernel-dynamic.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vnf-kernel-dynamic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
